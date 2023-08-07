ab679cc56601e85c3aba34a1670bf4e0485b81dc
Helam7u7 <helam053@gmail.com>
2023-08-06 19:02:04 -0500

    commit

---
+++ frontend/src/pages/ProductList.jsx
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+import { mobile } from "../responsive";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+  ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })}
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+  ${mobile({ marginRight: "0px" })}
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+  ${mobile({ margin: "10px 0px" })}
+`;
+const Option = styled.option``;
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Title>Ropa</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filtrar Producto:</FilterText>
+          <Select>
+            <Option disabled selected>
+              Color
+            </Option>
+            <Option>Blanco</Option>
+            <Option>Negro</Option>
+            <Option>Rojo</Option>
+            <Option>Azul</Option>
+            <Option>Amarillo</Option>
+            <Option>Verde</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Tamaño
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Ordenar por:</FilterText>
+          <Select>
+            <Option selected>más nuevo</Option>
+            <Option>Precio (asc)</Option>
+            <Option>Precio (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ProductList;
